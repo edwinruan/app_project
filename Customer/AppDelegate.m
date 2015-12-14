f9cb4b9f10b1b0146f7ea3c136103125dc268e91
edwinruan <edwinruan@gmail.com>
2015-12-13 23:12:35 -0600

    Update Appdelegate file
    add more comments to make it readable

--- Customer/AppDelegate.m
+++ Customer/AppDelegate.m
@@ -18,7 +18,7 @@
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
     _sharedSqlite = [CXSSqliteHelper sharedSqliteHelper];
-    
+    //singleton 
     return YES;
 }
 
